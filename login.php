19f87f6aaedb5c1bac99f93806ac4a100b96d43c
SriJahnavi-123 <129378852+SriJahnavi-123@users.noreply.github.com>
2023-07-14 18:22:14 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginPage</title>
+    <link rel="stylesheet" href="./login.css">
+</head>
+<body>
+  <form action="input.php">
+  <div class="container">
+    <h2>Login</h2>
+    <label for="username">Username</label>
+      <input type="text" id="username" name="username" placeholder="Enter username">
+
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" placeholder="Enter password">
+      <input type="radio">
+      <label for="">Agree with Terms and Conditions</label>
+      <button type="submit">Login</button>
+      <p>Not registered?<a href="#">Create an account</a></p>
+  </div>
+</form>
+</body>
+</html>
